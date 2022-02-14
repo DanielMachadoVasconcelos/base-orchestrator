78ffab0f6de9202683c02dc5ebc1733a77b41645
daniel.vasconcelos <daniel.vasconcelos@klarna.com>
2022-02-14 15:34:07 +0100

    Added the necessary infrastructure to perform black box tests

---
+++ integration-test/src/main/java/br/ead/home/IntegrationTestRunner.java
@@ -0,0 +1,11 @@
+package br.ead.home;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class IntegrationTestRunner {
+
+    public static void main(String[] args) {
+        log.info("Running Integration Tests ...");
+    }
+}
