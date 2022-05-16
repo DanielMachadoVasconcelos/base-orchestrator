2f249f8111cbd92398e829434ba52c11b1e97e95
Daniel Machado Vasconcelos <danielm.vasconcelos@gmail.com>
2022-05-16 08:50:31 +0200

    Added temporary hack to control the containers start up in the integration test

--- integration-test/startup.sh
+++ integration-test/startup.sh
@@ -3,9 +3,20 @@
 set -e
 JAR=$1
 
-while [[ "$(curl -k -s -o /dev/null -w ''%{http_code}'' http://sales:8082/actuator/health)" != "200" ]]; do
-  >&2 echo "Integration Test - Sales not started. Will sleep for 2 seconds."
-  sleep 2
+sleep 15
+
+while [[ "$(curl -k -s -o /dev/null -w ''%{http_code}'' http://elasticsearch:9200/_cluster/health?wait_for_status=yellow&timeout=2s)" != "200" ]]; do
+  >&2 echo "Integration Test - Elasticsearch not started. Will sleep for 10 seconds."
+  sleep 15
 done
+
+sleep 15
+
+while [[ "$(curl -k -s -o /dev/null -w ''%{http_code}'' http://sales:8082/actuator/health)" != "200" ]]; do
+  >&2 echo "Integration Test - Sales not started. Will sleep for 15 seconds."
+  sleep 15
+done
+
+sleep 15
 
 java -jar ${JAR}
--- sales/startup.sh
+++ sales/startup.sh
@@ -3,9 +3,9 @@
 set -e
 JAR=$1
 
-while [[ "$(curl -k -s -o /dev/null -w ''%{http_code}'' http://localhost:9200/_cluster/health?wait_for_status=yellow&timeout=2s)" != "200" ]]; do
-  >&2 echo "Sales - Elasticsearch not started. Will sleep for 2 seconds."
-  sleep 2
+while [[ "$(curl -k -s -o /dev/null -w ''%{http_code}'' http://elasticsearch:9200/_cluster/health?wait_for_status=yellow&timeout=2s)" != "200" ]]; do
+  >&2 echo "Sales - Elasticsearch not started. Will sleep for 10 seconds."
+  sleep 10
 done
 
 java -jar ${JAR}
